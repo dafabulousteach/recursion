3e35e6e8cbded12eb11262c6782df6566ac5f418
Kim Merino <dafabulousteach@gmail.com>
2015-06-24 19:54:05 -0700

    Full functionality added to getElementsById

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
+
+// THE CALL document.getElementsByClassName('targetClassName');
+
 var getElementsByClassName = function(className
 ){
-  // your code here
+  // className is passed
+  var child = document.body;
+  var matches = []; // instantiate a variable to hold the matching class names
+
+  if(child.getAttribute('class') === className){ // if the class name is equal to the target
+    matches.push(child);// push it to the array
+
+  }
+  
+    function digDeep(val){
+      for(var i = 0; i < val.childNodes.length; i++){
+        if(val.childNodes[i].hasChildNodes()){ // if there is a child node present
+          digDeep(val.childNodes[i]); // call digDeep on the child nodes
+        }
+        if($(val.childNodes[i]).hasClass(className)){ // if the class name is equal to the target
+          matches.push(val.childNodes[i]); // push it to the array
+        
+    }
+    
+
+      }
+    }
+    digDeep(child);
+    console.log(matches);
+    return matches;// return an array of the matching nodes
 };
+
+
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,6 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  return eval(json);
+	//console.log(json);
+  return eval('"' +json+'"');
 };
